09bf744561b5e140239b6175395da358c1271994
dominic.tobias <dominic.tobias@db.com>
2017-03-21 16:17:18 +0000

    Upgrade deps and webpack to v2

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-  entry: './lib/ReactCrop.jsx',
+  entry: './lib/ReactCrop',
   output: {
     path: path.join(__dirname, 'dist'),
     library: 'ReactCrop',
@@ -9,17 +9,13 @@ module.exports = {
     filename: 'ReactCrop.js',
   },
   externals: {
-    'react': 'react',
-    'object-assign': 'object-assign',
+    react: 'react',
   },
   module: {
     loaders: [{
       test: /\.(js|jsx)$/,
       exclude: /node_modules/,
-      loader: 'babel',
-    }]
-  },
-  resolve: {
-    extensions: ['', '.js', '.jsx']
+      loader: 'babel-loader',
+    }],
   },
 };
