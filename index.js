54e46ec430597ca3da19c9aa7fd9c40b6e72034c
Matt McFarland <contact@mattmcfarland.com>
2017-09-25 12:51:11 -0400

    Multiple fixes(see full commit message) Remove and replace createCropSelection with a React Component that is extensible, and add it to the exports list. export all the things for commonjs, and export the same default Re-add event.target checks against this.refXXXX instead of stopPropagation per conversation with Dom Update version in package.json

--- index.js
+++ index.js
@@ -1,1 +1,3 @@
-module.exports = require('./dist/ReactCrop');
+module.exports = require('./dist/ReactCrop').ReactCrop;
+module.exports.CropSelection = require('./dist/CropSelection').CropSelection;
+module.exports.withCrop = require('./dist/ReactCrop').withCrop;
