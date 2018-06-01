afddbaf28d5d4ceb81573d262efc5840099ea25c
Dominic Tobias <dominic.tobias@gmail.com>
2018-06-01 19:24:21 +0100

    Use ES6 export to fix issue with no export named "default" for es6 default importing #178

--- dist/ReactCrop.js
+++ dist/ReactCrop.js
@@ -460,6 +460,11 @@ module.exports = warning;
 "use strict";
 
 
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.makeAspectCrop = exports.getPixelCrop = exports.default = exports.ReactCrop = undefined;
+
 var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
 var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; }; /* globals document, window */
@@ -1264,10 +1269,10 @@ ReactCrop.defaultProps = {
   imageStyle: undefined
 };
 
-module.exports = ReactCrop;
-module.exports.getPixelCrop = getPixelCrop;
-module.exports.makeAspectCrop = makeAspectCrop;
-module.exports.containCrop = containCrop;
+exports.ReactCrop = ReactCrop;
+exports.default = ReactCrop;
+exports.getPixelCrop = getPixelCrop;
+exports.makeAspectCrop = makeAspectCrop;
 
 /***/ }),
 /* 6 */
