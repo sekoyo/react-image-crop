b552f7a29c9b0b4b9f697807d5a2678f2f5dd004
Dominic <dominic.tobias@gmail.com>
2021-06-14 22:59:43 +0800

    Switch to pointer events. Remove support for proprietary IE focus func. Change to touch-action: none to prevent scrolling during touch move.

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,8 +12,6 @@ module.exports = {
     'arrow-parens': 0,
     'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
     'react/destructuring-assignment': 0,
-    'jsx-a11y/tabindex-no-positive': 0,
-    'jsx-a11y/no-noninteractive-tabindex': 0,
     'react/jsx-wrap-multilines': 0,
   },
 };
