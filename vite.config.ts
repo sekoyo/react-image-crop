e84152b7954d40c7ba861dea756959f36332b7d2
Kevin Ross <kevin.ross@alienfast.com>
2023-05-27 13:32:58 -0500

    Fix types missing in new vite build (#542)

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { resolve } from 'path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import dts from 'vite-plugin-dts'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -37,5 +38,6 @@ export default defineConfig({
     react({
       jsxRuntime: 'classic',
     }),
+    dts(),
   ],
 })
